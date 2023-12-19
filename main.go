b047bb3c93da1008a29d93425816a6bfef0175b1
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2023-12-19 17:35:09 +0000

    Fix

--- main.go
+++ main.go
@@ -16,42 +16,29 @@ import (
 	fcgiclient "github.com/tomasen/fcgi_client"
 )
 
-// PHPFPMStatus is a struct that represents the JSON response from PHP-FPM status endpoint.
-// We only need a few fields, but the rest are here for reference. See https://www.php.net/manual/en/fpm.status.php for more information.
-type PHPFPMStatus struct {
-	Pool            string          `json:"pool"`
-	ProcessManager  string          `json:"process manager"`
-	StartTime       int64           `json:"start time"`
-	StartSince      int64           `json:"start since"`
-	AcceptedConn    int64           `json:"accepted conn"`
-	ListenQueue     int64           `json:"listen queue"`
-	MaxListenQueue  int64           `json:"max listen queue"`
-	ListenQueueLen  int64           `json:"listen queue len"`
-	IdleProcesses   int64           `json:"idle processes"`
-	ActiveProcesses int64           `json:"active processes"`
-	TotalProcesses  int64           `json:"total processes"`
-	MaxActiveProcs  int64           `json:"max active processes"`
-	MaxChildren     int64           `json:"max children reached"`
-	SlowRequests    int64           `json:"slow requests"`
-	Processes       []PHPFPMProcess `json:"processes"`
+// MetadataResponse is a struct that represents the JSON response from the ECS metadata endpoint.
+// See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html for more information.
+type MetadataResponse struct {
+	ServiceName string `json:"ServiceName"`
 }
 
-// PHPFPMProcess is a struct that represents a process in the JSON response from PHP-FPM status endpoint.
-// We don't use any of these fields but they may be useful in the future.
-type PHPFPMProcess struct {
-	PID             int64   `json:"pid"`
-	State           string  `json:"state"`
-	StartTime       int64   `json:"start time"`
-	StartSince      int64   `json:"start since"`
-	Requests        int64   `json:"requests"`
-	RequestDuration int64   `json:"request duration"`
-	RequestMethod   string  `json:"request method"`
-	RequestURI      string  `json:"request uri"`
-	ContentLength   int64   `json:"content length"`
-	User            string  `json:"user"`
-	Script          string  `json:"script"`
-	LastRequestCPU  float64 `json:"last request cpu"`
-	LastRequestMem  int64   `json:"last request memory"`
+// PHPFPMStatus is a struct that represents the JSON response from PHP-FPM status endpoint.
+// See https://www.php.net/manual/en/fpm.status.php for more information.
+type PHPFPMStatus struct {
+	Pool            string `json:"pool"`
+	ProcessManager  string `json:"process manager"`
+	StartTime       int64  `json:"start time"`
+	StartSince      int64  `json:"start since"`
+	AcceptedConn    int64  `json:"accepted conn"`
+	ListenQueue     int64  `json:"listen queue"`
+	MaxListenQueue  int64  `json:"max listen queue"`
+	ListenQueueLen  int64  `json:"listen queue len"`
+	IdleProcesses   int64  `json:"idle processes"`
+	ActiveProcesses int64  `json:"active processes"`
+	TotalProcesses  int64  `json:"total processes"`
+	MaxActiveProcs  int64  `json:"max active processes"`
+	MaxChildren     int64  `json:"max children reached"`
+	SlowRequests    int64  `json:"slow requests"`
 }
 
 // Export metrics to CloudWatch. If we can't export them, we'll log an error.
@@ -147,16 +134,15 @@ func retrievePhpFpmStats() PHPFPMStatus {
 	return response
 }
 
-// Retrieve service name of the ECS task from metadata endpoint. If we can't retrieve it, we'll return "NOT_SET" and log an error message.
+// Retrieve service name of the ECS task from metadata endpoint.
 func retrieveServiceName() string {
 	serviceName := "NOT_SET"
 	metadataUrl := os.Getenv("ECS_CONTAINER_METADATA_URI_V4")
-	infoEndpoint := metadataUrl + "/latest/meta-data/instance-id"
+	infoEndpoint := metadataUrl + "/task"
 
 	request, err := http.NewRequest("GET", infoEndpoint, nil)
 	if err != nil {
 		log.Printf("Error creating request: %v. Defaulting to %v", err, serviceName)
-		return serviceName
 	}
 
 	request.Header.Set("X-Aws-Ec2-Metadata-Token-Ttl-Seconds", "21600")
@@ -170,18 +156,22 @@ func retrieveServiceName() string {
 
 	defer response.Body.Close()
 
-	body, err := io.ReadAll(response.Body)
+	// Read the JSON response and extract the ServiceName value
+
+	metadataResponse := new(MetadataResponse)
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&metadataResponse)
+
 	if err != nil {
-		log.Printf("Error reading response body: %v. Defaulting to %v", err, serviceName)
-		return serviceName
+		log.Printf("Error decoding response body: %v. Defaulting to %v", err, serviceName)
 	}
 
 	if response.StatusCode != 200 {
 		log.Printf("Received non-200 response from meta-data URL: %v. Defaulting to %v", response.StatusCode, serviceName)
-		return serviceName
 	}
 
-	serviceName = string(body)
+	serviceName = string(metadataResponse.ServiceName)
+
 	return serviceName
 }
 
--- main_test.go
+++ main_test.go
@@ -9,40 +9,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRetrieveServiceName(t *testing.T) {
-	// Test with a valid ECS_CONTAINER_METADATA_URI_V4 environment variable set (should return test-instance-id)
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func createMockServer() *httptest.Server {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response := `{"ServiceName": "test-service-name"}`
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("test-instance-id"))
-	}))
-	defer server.Close()
+		w.Write([]byte(response))
+	})
 
-	// Replace the infoEndpoint with the test server URL
-	os.Setenv("ECS_CONTAINER_METADATA_URI_V4", server.URL)
-
-	// Call the retrieveInstanceId function
-	instanceId := retrieveServiceName()
-
-	// Verify the result
-	expectedInstanceId := "test-instance-id"
-	assert.Equal(t, expectedInstanceId, instanceId, "Expected instance ID: %s, but got: %s", expectedInstanceId, instanceId)
+	return httptest.NewServer(handler)
 }
 
-func TestRetrieveServiceNameNoServer(t *testing.T) {
-	// Test with no ECS_CONTAINER_METADATA_URI_V4 environment variable set (should return NOT_SET)
-	os.Unsetenv("ECS_CONTAINER_METADATA_URI_V4")
-	instanceId := retrieveServiceName()
-	assert.Equal(t, "NOT_SET", instanceId, "Expected instance ID: NOT_SET, but got: %s", instanceId)
-}
-
-func TestRetrieveServiceNameBadServer(t *testing.T) {
-	// Test with a bad ECS_CONTAINER_METADATA_URI_V4 environment variable set (should return a 500 error)
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusInternalServerError)
-	}))
+func TestRetrieveServiceName(t *testing.T) {
+	server := createMockServer()
 	defer server.Close()
 
 	os.Setenv("ECS_CONTAINER_METADATA_URI_V4", server.URL)
-	instanceId := retrieveServiceName()
-	assert.Equal(t, "NOT_SET", instanceId, "Expected instance ID: NOT_SET, but got: %s", instanceId)
+
+	serviceName := retrieveServiceName()
+
+	expectedServiceName := "test-service-name"
+	assert.Equal(t, expectedServiceName, serviceName, "Expected service name: %s, but got: %s", expectedServiceName, serviceName)
 }
